3019d97847a11b5baf6e72314d2c09d4f34c2ba2
no-shin <irisinoirisino@gmail.com>
2025-02-16 13:41:00 +0900

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/AtCoder-Games/',
+  base: './',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
